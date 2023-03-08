075f4a345f0dd47af4c59b6c7fb5198945a1eae4
Angielyn Rivera <2194059@cognizant.com>
2023-03-08 16:52:21 +0800

    Added links to menu

--- src/components/Navigation/TopNavigation.js
+++ src/components/Navigation/TopNavigation.js
@@ -1,5 +1,7 @@
 import React from "react";
-import { AppBar, Toolbar, Typography } from "@mui/material";
+import { AppBar, Toolbar, Typography, Box } from "@mui/material";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
 const drawerWidth = 240;
 
@@ -8,12 +10,33 @@ const TopNavigation = () => {
     <div>
       <AppBar
         position="fixed"
-        sx={{ width: `calc(100% - ${drawerWidth}px)`, ml: `${drawerWidth}px` }}
+        sx={{
+          width: `calc(100% - ${drawerWidth}px)`,
+          ml: `${drawerWidth}px`,
+        }}
       >
-        <Toolbar>
+        <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
           <Typography variant="h6" noWrap component="div">
             ReactJS Sandbox
           </Typography>
+          <Box>
+            <a
+              href="https://ph.linkedin.com/in/angielynrivera"
+              target="_blank"
+              rel="noreferrer"
+              style={{ textDecoration: "none", color: "#fff" }}
+            >
+              <LinkedInIcon sx={{ marginRight: "10px" }}></LinkedInIcon>
+            </a>
+            <a
+              href="https://github.com/angielynr"
+              target="_blank"
+              rel="noreferrer"
+              style={{ textDecoration: "none", color: "#fff" }}
+            >
+              <GitHubIcon></GitHubIcon>
+            </a>
+          </Box>
         </Toolbar>
       </AppBar>
     </div>
